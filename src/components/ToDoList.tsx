4a68bf772dcb437130fab1b007df5ce1fced136c
Daniel Ruiz <danuruiz7@hotmail.com>
2023-09-09 00:51:10 +0200

    conectado al backend de django

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,16 +1,16 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import './ToDoList.css';
 
 interface Item {
-  id: any;
+  id: null;
   name: string;
-  cantidad: string;
+  quantity: number;
 }
 
 const inicial: Item = {
   id: null,
   name: '',
-  cantidad: '1',
+  quantity: 1,
 };
 
 const ToDoList = () => {
@@ -18,6 +18,27 @@ const ToDoList = () => {
   const [list, setList] = useState<Item[]>([]);
   const [showForm, setShowForm] = useState(false);
   const [showList, setShowList] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/items/');
+
+        if (!response.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+
+        const data = await response.json();
+        setList(data);
+      } catch (err) {
+        console.error('Error al obtener los datos', err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(list);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setForm({
@@ -31,10 +52,25 @@ const ToDoList = () => {
     setForm(item);
   };
 
-  const handleDelete = (name: string) => {
-    console.log(name);
-    const newList = list.filter((item) => item.name !== name);
-    setList(newList);
+  const handleDelete = (id: any) => {
+    fetch(`http://127.0.0.1:8000/api/items/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        throw new Error('La solicitud no fue exitosa');
+      }
+      return res.json();
+    })
+    .then((data) => {
+      setList(data);
+      setForm(inicial);
+    })
+    .catch((err) => {
+      console.error('Error al obtener los datos', err);
+    });
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -44,32 +80,63 @@ const ToDoList = () => {
     if (!form.name) {
       alert('El nombre no puede estar vacio');
       return;
-    } else if (form.id === null) {
-      form.id = Date.now();
-
-      setList([...list, form]);
-      setForm(inicial);
-      return;
-    } else {
-      const newList = list.map((item) => {
-        if (item.id === form.id) {
-          item.name = form.name;
-          item.cantidad = form.cantidad;
-        }
-        return item;
-      });
-      setList(newList);
-      setForm(inicial);
-      console.log(newList, 'new list');
-
+    } else if (form.quantity < 1) {
+      alert('La cantidad no puede ser menor a 1');
       return;
     }
+
+    if (!form.id) {
+    fetch('http://127.0.0.1:8000/api/items/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setList(data);
+        setForm(inicial);
+      })
+      .catch((err) => {
+        console.error('Error al obtener los datos', err);
+      });
+    }else{
+      fetch(`http://127.0.0.1:8000/api/items/${form.id}/`,{
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(form)
+      }).then((res) => {
+        if (!res.ok) {
+          throw new Error('La solicitud no fue exitosa');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setList(data);
+        setForm(inicial);
+      })
+      .catch((err) => {
+        console.error('Error al obtener los datos', err);
+      });
+    }
+
+
+
+
   };
 
   return (
     <div className="container">
       <h3 className="title">To Do List</h3>
-      <form action="" onSubmit={handleSubmit}>
+      <form method="POST" onSubmit={handleSubmit}>
         <div className="title-container">
           <h3 className="title-form">Agrega un producto </h3>
           <button
@@ -85,7 +152,7 @@ const ToDoList = () => {
           </button>
         </div>
 
-        <div className={showForm ? 'form-info' : 'form-info hidden'}>
+        <div className={showForm ? 'form-info' : 'form-info'}>
           <div className="form-group">
             <label htmlFor="">Nombre:</label>
             <input
@@ -98,13 +165,13 @@ const ToDoList = () => {
           </div>
 
           <div className="form-group">
-            <label htmlFor="">Cantidad</label>
+            <label htmlFor="">quantity</label>
             <input
               type="number"
               placeholder="Ejemplo: 10"
-              name="cantidad"
+              name="quantity"
               onChange={handleChange}
-              value={form.cantidad}
+              value={form.quantity}
             />
           </div>
           <button className="btn-form">Agregar</button>
@@ -125,11 +192,11 @@ const ToDoList = () => {
             )}
           </button>
         </div>
-        <ul className={showList ? '' : 'hidden'}>
+        <ul className={showList ? '' : ''}>
           {list.map((item) => (
             <li className="item" key={item.id}>
               <div className="item-info">
-                <span className="item-cantidad-">{item.cantidad}</span>{' '}
+                <span className="item-quantity-">{item.quantity}</span>{' '}
                 <span className="item-name">{item.name}</span>
               </div>
               <div className="item-botones" onClick={() => handleEdit(item)}>
@@ -138,7 +205,7 @@ const ToDoList = () => {
                 </button>
                 <button
                   className="item-delete"
-                  onClick={() => handleDelete(item.name)}
+                  onClick={() => handleDelete(item.id)}
                 >
                   <i className="bi bi-trash3-fill"></i>
                 </button>
